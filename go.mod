d95c0070db35b2b80feeeb2cb5977b942e44449f
Vinicius Ferreira Gomes <vini.ferr33@gmail.com>
2023-12-22 19:39:46 -0300

    fix: docker compose test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module wondrousBaazar
 
 go 1.21
 
-require github.com/go-chi/chi/v5 v5.0.10 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/lib/pq v1.10.9
+)
--- postgres/item_repository_test.go
+++ postgres/item_repository_test.go
@@ -1,1 +1,32 @@
 package postgres
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	db, err = sql.Open("postgres", "user=test password=password dbname=test sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exitCode := m.Run()
+
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(exitCode)
+}
+
+func TestItemRepository_Save(t *testing.T) {
+
+}
